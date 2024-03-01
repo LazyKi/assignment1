283fd77eca68cd1bd025c08b1161e3c19f3529a6
LazyKi <lanwanghuang114514@gmail.com>
2024-03-01 23:36:26 +0800

    窗体应用

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace jisuan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.textBoxNum2 = new System.Windows.Forms.TextBox();
+            this.textBoxNum1 = new System.Windows.Forms.TextBox();
+            this.operatorComboBox = new System.Windows.Forms.ComboBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(431, 213);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(94, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxNum2
+            // 
+            this.textBoxNum2.Location = new System.Drawing.Point(502, 275);
+            this.textBoxNum2.Name = "textBoxNum2";
+            this.textBoxNum2.Size = new System.Drawing.Size(137, 25);
+            this.textBoxNum2.TabIndex = 1;
+            this.textBoxNum2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxNum1
+            // 
+            this.textBoxNum1.Location = new System.Drawing.Point(309, 275);
+            this.textBoxNum1.Name = "textBoxNum1";
+            this.textBoxNum1.Size = new System.Drawing.Size(127, 25);
+            this.textBoxNum1.TabIndex = 2;
+            // 
+            // operatorComboBox
+            // 
+            this.operatorComboBox.FormattingEnabled = true;
+            this.operatorComboBox.Location = new System.Drawing.Point(204, 347);
+            this.operatorComboBox.Name = "operatorComboBox";
+            this.operatorComboBox.Size = new System.Drawing.Size(121, 23);
+            this.operatorComboBox.TabIndex = 3;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(463, 347);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 15);
+            this.resultLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 479);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.operatorComboBox);
+            this.Controls.Add(this.textBoxNum1);
+            this.Controls.Add(this.textBoxNum2);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox textBoxNum2;
+        private System.Windows.Forms.TextBox textBoxNum1;
+        private System.Windows.Forms.ComboBox operatorComboBox;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
